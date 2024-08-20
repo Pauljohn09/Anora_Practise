aa04125045976d54bede255c5ba204d7f214eaae
Surenthar S <surenthars@anorasolutions.com>
2024-08-20 14:58:35 +0530

    Update and rename Surenthar to C2file.c

---
+++ C2file.c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{
+    printf("Hello World");
+    printf("Hello World - copy");
+printf("Hello World");
+    printf("Hello World - copy");
+    printf("Hello World");
+    printf("Hello World - printf ;copy");
+    printf("Hello World");
+    printf("Hello World - copy");
+printf("Hello World");
+    printf("Hello World - copy");
+    printf("Hello World");
+    printf("Hello World - printf ;copy");
+  printf("Hello World");
+    printf("Hello World - copy");
+    printf("Hello World");
+    printf("Hello World - printf ;copy");
+    return 0;
+}
